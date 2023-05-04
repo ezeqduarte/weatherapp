f47413687f4fa18b933d0c57960a1147d865097c
ezeqduarte <ezeqduarte01@gmail.com>
2023-05-04 11:15:22 -0300

    function of conditions

--- src/functions/functions.js
+++ src/functions/functions.js
@@ -10,22 +10,74 @@ const getCurrentLocation = (setLatitude, setLongitude) => {
 };
 
 function getDay(date) {
-    const diasDeLaSemana = [
-      "Domingo",
-      "Lunes",
-      "Martes",
-      "Miércoles",
-      "Jueves",
-      "Viernes",
-      "Sábado",
-    ];
-    const newDate = new Date(date);
-    const dia = newDate.getDay();
-    return diasDeLaSemana[dia]
+  const diasDeLaSemana = [
+    "Domingo",
+    "Lunes",
+    "Martes",
+    "Miércoles",
+    "Jueves",
+    "Viernes",
+    "Sábado",
+  ];
+  const newDate = new Date(date);
+  const dia = newDate.getDay();
+  return diasDeLaSemana[dia];
+}
+
+function getMonth(month) {
+  const mesesDelAño = [
+    "Enero",
+    "Febrero",
+    "Marzo",
+    "Abril",
+    "Mayo",
+    "Junio",
+    "Julio",
+    "Agosto",
+    "Septiembre",
+    "Octubre",
+    "Noviembre",
+    "Diciembre",
+  ];
+  if (month < 10) {
+    month = month.slice(1);
+  }
+  return mesesDelAño[month - 1];
+}
+
+function getInfoHours(hour) {
+  if (0 <= hour && hour < 12) {
+    return `${hour} AM`;
+  }
+  if (12 <= hour && hour < 24) {
+    return `${hour} PM`;
+  }
+}
+
+function getConditionsSem(actuallyHour, array) {
+  const arrayOfConditions = array[0].hour.filter(
+    (hours, index) => index >= actuallyHour
+  );
+
+  if (arrayOfConditions.length < 12) {
+    array[1].hour.map((hour) => {
+      if (arrayOfConditions.length < 12) {
+        arrayOfConditions.push(hour);
+      }
+    });
   }
 
+  console.log(arrayOfConditions);
+
+  return arrayOfConditions;
+}
+
 const functions = {
-  getCurrentLocation, getDay
+  getCurrentLocation,
+  getDay,
+  getMonth,
+  getInfoHours,
+  getConditionsSem,
 };
 
 export default functions;
--- src/pages/index.js
+++ src/pages/index.js
@@ -9,7 +9,13 @@ export default function Home() {
   const [data, setData] = useState({});
 
   const { current_local: petition_local } = api;
-  const { getCurrentLocation, getDay } = functions;
+  const {
+    getCurrentLocation,
+    getDay,
+    getMonth,
+    getInfoHours,
+    getConditionsSem,
+  } = functions;
   const { location, current, forecast } = data;
 
   useEffect(() => {
@@ -28,16 +34,32 @@ export default function Home() {
         >
           {/* este div es mi celular */}
 
-          <div className="celphone w-128 bg-white overflow-auto shadow-2xl h-full p-5 border rounded">
+          <div className="celphone relative w-128 bg-white overflow-auto shadow-2xl h-full p-5 border rounded">
+            {/* barra del celular */}
+            <div className="flex justify-between bg-blue-200 absolute top-0 left-0 px-2 w-full bg-gray-100">
+              <div>
+                <p className="text-gray-400 font-semibold">
+                  {current.last_updated.slice(10, 16)}
+                </p>
+              </div>
+              <div>
+                <p className="text-gray-400 font-semibold">72%</p>
+              </div>
+            </div>
             {/* header */}
 
-            <header className="w-100 bg-white pt-5 mb-5">
+            <header className="w-100 mt-5 text-end bg-white pt-5 mb-5">
               <h1 className="text-5xl font-bold truncate">{location.name}</h1>
               <span className="text-3xl truncate font-thin italic font-bold">
                 {location.region}, {location.country}
               </span>
               <p className="truncate font-thin font-bold">
-                {getDay(current.is_day)} {current.last_updated}
+                Last updated:
+                {` `}
+                {getDay(current.is_day)} {current.last_updated.slice(8, 10)} de{" "}
+                {``}
+                {getMonth(current.last_updated.slice(5, 7))}
+                {current.last_updated.slice(10, 16)}
               </p>
             </header>
             {/* primer div con informacion del clima */}
@@ -94,59 +116,29 @@ export default function Home() {
                 </div>
               </div>
             </div>
-            
+
             {/* segundo div con informacion del clima */}
             <div className=" rounded px-5 py-8 mt-5 bg-sky-50 ">
-              <div className="flex items-center">
-                <h2 className="text-7xl font-extrabold border-r-2 pr-3">
-                  {current.temp_c}º
-                </h2>
-                <div className="pl-3 flex flex-col font-semibold justify-between text-lg">
-                  <p>Max temp {current.temp_c}º</p>
-                  <p>Min temp {current.temp_c}º</p>
-                </div>
-              </div>
-              <div className="flex items-center">
-                <p className="mt-1 italic">{current.condition.text} </p>
-                <img className="w-8 ml-1" src={current.condition.icon}></img>
-              </div>
-              <p className="italic">Feels like: {current.feelslike_c}º</p>
-              <p className="italic">
-                Air quality: {current.air_quality["us-epa-index"]}
-              </p>
-              <p className="italic">Yellow warning for storms </p>
-              <hr className="my-5"></hr>
-              <div className="w-full flex">
-                {forecast.forecastday.map((day) => (
-                  <div className="py-3  rounded mx-1 bg-white flex flex-col items-center justify-center w-full">
-                    <p className="uppercase font-medium">
-                      {getDay(day.date).slice(0, 3)}
-                    </p>
-                    <img className="w-10" src={day.day.condition.icon}></img>
-                    <p>{parseInt(day.day.maxtemp_c)}º</p>
-                    <p className="text-gray-400">
-                      {parseInt(day.day.mintemp_c)}º
-                    </p>
+              <h2 className="uppercase text-xl font-semibold">
+                {getDay(current.is_day)}, {current.last_updated.slice(8, 10)} de{" "}
+                {` `}
+                {getMonth(current.last_updated.slice(5, 7))}
+              </h2>
+              <p>Updated now</p>
+              <div className="flex flex-col gap-3 mt-3">
+                {getConditionsSem(17, forecast.forecastday).map((hour) => (
+                  <div className="flex w-100 items-center justify-between bg-white">
+                    <p>{getInfoHours(hour.time.slice(10, 13))}</p>
+
+                    <img
+                      className="w-8 ml-1"
+                      src={current.condition.icon}
+                    ></img>
+                    <p>31º</p>
+                    <p>0%</p>
+                    <p>22.2 km/h</p>
                   </div>
                 ))}
-              </div>
-              <div className="w-full text-sky-900  gap-3 mt-3 flex">
-                <div className=" rounded mx-1 bg-white flex flex-col items-center justify-center w-full">
-                  <p className=" font-semibold text-xl">
-                    H {current.humidity}%
-                  </p>
-                  <img className="w-10"></img>
-                </div>
-                <div className=" rounded mx-1 bg-white flex flex-col items-center justify-center w-full">
-                  <p className=" font-semibold text-xl">
-                    P {current.precip_mm} mmHg
-                  </p>
-                  <img className="w-10"></img>
-                </div>
-                <div className="py-2 rounded mx-1 bg-white flex flex-col items-center justify-center w-full">
-                  <p className=" font-semibold text-xl">UV {current.uv}</p>
-                  <img className="w-10"></img>
-                </div>
               </div>
             </div>
           </div>
